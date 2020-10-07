3f0fc5f0af60943e9072f208356b7a6895ec304c
SAHIL DHOLPURIA <46110596+Sahildholpuria@users.noreply.github.com>
2020-10-07 18:58:26 +0530

    commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 print("Enter Web Site URL like( https://sahildholpuria.tech/ ) to Get HTML Source Code. \n")
 WebName = str(input("Enter URL here : "))
 
-browser = webdriver.Chrome(executable_path="C:\drivers\chromedriver.exe")
+browser = webdriver.Chrome(executable_path="./drivers\chromedriver.exe")
 browser.get(WebName)
 
 f = open('./WebSourceCode.txt','w')
